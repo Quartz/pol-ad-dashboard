028537180324ae8c21f24a6de34d4e1afacc6382
Jeremy Merrill <jmerrill@qz.com>
2020-04-22 19:16:38 -0400

    in Tools, it's a link to past 3 months; adds BigSpenders link

--- src/components/Tools/index.jsx
+++ src/components/Tools/index.jsx
@@ -6,12 +6,13 @@ const cx = classnames.bind( styles );
 
 const Tools = () => (
 <div className={cx( 'tools' )}>
+  <li href="https://dashboard-backend.qz.ai/bigspenders">Big spenders this week</li>
   <h4>Advertisers</h4>
   <ul>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=week&time_count=1&first_seen=1">New this week</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=week&time_count=1">Seen this week</a></li>
-    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=month&time_count=3&first_seen=1">New this month</a></li>
-    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=month&time_count=3">Seen this month</a></li>
+    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=month&time_count=3&first_seen=1">New past 3 months</a></li>
+    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=month&time_count=3">Seen past 3 months</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser?time_unit=electioncycle&time_count=1">Seen this election cycle</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/advertiser">Seen ever</a></li>
   </ul>
@@ -19,8 +20,8 @@ const Tools = () => (
   <ul>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=week&time_count=1&first_seen=1">New this week</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=week&time_count=1">Seen this week</a></li>
-    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=month&time_count=3&first_seen=1">New this month</a></li>
-    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=month&time_count=3">Seen this month</a></li>
+    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=month&time_count=3&first_seen=1">New past 3 months</a></li>
+    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=month&time_count=3">Seen past 3 months</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by?time_unit=electioncycle&time_count=1">Seen this election cycle</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/paid_for_by">Seen ever</a></li>
   </ul>
@@ -28,8 +29,8 @@ const Tools = () => (
   <ul>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=week&time_count=1&first_seen=1">New this week</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=week&time_count=1">Seen this week</a></li>
-    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=month&time_count=3&first_seen=1">New this month</a></li>
-    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=month&time_count=3">Seen this month</a></li>
+    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=month&time_count=3&first_seen=1">New past 3 months</a></li>
+    <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=month&time_count=3">Seen past 3 months</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments?time_unit=electioncycle&time_count=1">Seen this election cycle</a></li>
     <li><a href="https://dashboard-backend.qz.ai/ads/pivot/segments">Seen ever</a></li>
   </ul>
